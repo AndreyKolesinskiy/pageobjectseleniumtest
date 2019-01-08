e35bd20dfa75ccba6207b9fac87cb6178255af79
AndyRivers47 <andykolesinski@gmail.com>
2019-01-08 22:56:03 +0300

    no message

---
+++ main/java/SignUpPage.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignUpPage {
+
+    private WebDriver driver;
+
+
+    public SignUpPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By signInButton = By.xpath("//a[@class='HeaderMenu-link no-underline mr-3']");
+
+
+
+
+
+
+
+}
